391eb8c46ee6601abbd35158ddd0e2e666155661
Irina <irina.m.protasevich@gmail.com>
2022-12-18 17:37:33 +0400

    fead: add solution to Task02 'Operators'

---
+++ src/lesson02/Task02.java
@@ -0,0 +1,22 @@
+package lesson02;
+import java.util.Scanner;
+
+public class Task02 {
+    public static void main(String[] args){
+        /*int w = 2;
+        int h = 4;
+        System.out.println("Perimeter:" + 2*(w+h));
+        System.out.println("Square:" + w*h);*/
+        double w;
+        double h;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите ширину:");
+        w = Double.parseDouble(in.nextLine());
+        System.out.println("Введите высоту:");
+        h = Double.parseDouble(in.nextLine());
+        System.out.println("Вывести периметр:" + 2*(w+h));
+        System.out.println("Вывести площадь:" + w*h);
+
+
+    }
+}
